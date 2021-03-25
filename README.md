cbbeff5ce59499779f06477f82639f812a8afd4d
bjwlknsn <bjwlknsn@gmail.com>
2021-03-24 23:36:01 -0600

    Completed app.js. Minor modifications to index.html. Added text to README.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # javascript-challenge
+
+The group `ALIENS-R-REAL` have collected eye-witness reports of UFOs. The group requested code that will create a table dynamically based upon the data provided. They also needed their users to filter the table data by date. Per their request, the code could only use pure JavaScript, HTML, CSS, and D3.js. 
+
+### Automatic Table and Date Search
+
+* Created a basic HTML web page using the index.html file provided as a starting point.
+
+* Using the UFO dataset provided in the form of an array of JavaScript objects, wrote code that appends a table to the web page, and then adds new rows of data for each UFO sighting.
+
+  * The table has a column for `date/time`, `city`, `state`, `country`, `shape`, and `comment`.
+
+* A date form was used in the HTML document and wrote JavaScript code that will filter for events and search through the `date/time` column to find rows that match user input.
--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,62 @@
-// from data.js
+// Assign the data from `data.js` to a descriptive variable
 var tableData = data;
 
-// YOUR CODE HERE!
+// Set the reference to the table body, button and form
+var tbody = d3.select("tbody");
+var button = d3.select("button");
+var form = d3.select("form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Use d3 to automatically populate tableData
+function defaultPopulate(tableData) {
+    tableData.forEach((ufoSighting) => {
+        var row = tbody.append("tr");
+        Object.entries(ufoSighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
+
+defaultPopulate(tableData);
+
+// Complete the event handler function for the form
+function runEnter() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+  
+    // Use the form input to filter the data by date
+    var filteredData = tableData.filter(ufoSighting => ufoSighting.datetime === inputValue);
+    console.log(filteredData);
+
+    // Reset the previous filtered data
+    tbody.html("");
+    
+    // Display filtered data
+    filteredData.forEach((ufoSighting) => {
+        var row = tbody.append("tr");
+        Object.entries(ufoSighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+    
+    // If no value entered, autopopulate the entire data table
+    if (inputValue === "") {
+        defaultPopulate(tableData);
+    };
+    
+    // If no data found, display message
+    if (filteredData.length === 0) {
+        tbody.append("tr").text("No UFO sightings for that date. Search again!");
+    };    
+};
